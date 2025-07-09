74b754764ad8c7aeb4179e267607a9c4689d9ec3
Leo Cifuentes <leocifuentesp@gmail.com>
2025-07-09 16:31:28 -0500

    Primeros pasos con TypeScript

---
+++ variable_base/main.js
@@ -0,0 +1,9 @@
+var nombre = "Leo";
+var edad = 37;
+var esDesarrollador = true;
+var mes;
+var variableNull = null;
+console.log("hola, ".concat(nombre));
+console.log('Mes inicial: ', mes);
+mes = 'Julio';
+console.log('Mes actualizado: ', mes);
---
+++ variable_base/main.ts
@@ -0,0 +1,12 @@
+let nombre: string = "Leo";
+let edad: number = 37;
+let esDesarrollador: boolean = true;
+let mes: string | undefined;
+let variableNull: null = null;
+
+console.log(`hola, ${nombre}`);
+console.log('Mes inicial: ', mes);
+
+mes = 'Julio';
+
+console.log('Mes actualizado: ', mes);
